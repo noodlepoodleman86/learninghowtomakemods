27a31d1d10660695fa424b8b64702b83f9d39748
NoodlePoodle <noodlepoodle86@gmail.com>
2025-04-30 00:51:55 -0400

    Actual inital commit

---
+++ src/main/java/net/noodles/coolmod/CoolModClient.java
@@ -0,0 +1,10 @@
+package net.noodles.coolmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoolModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
