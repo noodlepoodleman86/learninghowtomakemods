d9008466471a15b177d39f07e6cc5e2f73c97dde
noodle <noodlepoodle86@gmail.com>
2025-05-11 18:48:23 -0400

    Actual inital commit

--- src/main/java/net/noodles/coolmod/block/ModBlocks.java
+++ src/main/java/net/noodles/coolmod/block/ModBlocks.java
@@ -4,20 +4,24 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.noodles.coolmod.CoolMod;
 
 
 public class ModBlocks {
 
     public static final Block OMNI_BLOCK = registerBlock("omni_block", new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
-
-
+    public static final Block MEME_ORE = registerBlock("meme_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(2.5f), UniformIntProvider.create(2,5)));
+    public static final Block DEEPSLATE_MEME_ORE = registerBlock("deepslate_meme_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE).strength(4f), UniformIntProvider.create(2,5)));
     private  static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(CoolMod.MOD_ID, name), block);
